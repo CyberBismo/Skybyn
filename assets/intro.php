21b590f321de37181fed290eb1c73116d441b395
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2023-12-27 18:54:13 +0100

    Update intro.php

--- assets/intro.php
+++ assets/intro.php
@@ -3,5 +3,12 @@
     <p>CLICK TO READ MORE</p>
     <br><br>
     This network is created to make a safer online community.<br>A place where you can feel free, be yourself, and have full control.<br>
-    You decide what you want to see, and who can see you.
+    You decide what you want to see, and who can see you.<br>
+    <br>
+    <h4>Our KEY focus</h4>
+    <ul>
+        <li><i class="fa-solid fa-shield"></i><b>Safety & Security</b></li>
+        <li><i class="fa-solid fa-person-breastfeeding"></i><b>Family</b></li>
+        <li><i class="fa-solid fa-user-tie"></i><b>Business</b></li>
+    </ul>
 </div>
