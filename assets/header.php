0e1cb098374dc5d520d19d380fd06ca8f4ad1b72
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-01-31 12:54:52 +0100

    Update header.php

--- assets/header.php
+++ assets/header.php
@@ -32,10 +32,39 @@ if ($currentUrl == $devDomain) {
         <script src="assets/js/scripts_logged.js"></script>
         <script>
             document.addEventListener("DOMContentLoaded", function() {
-                var img = document.getElementsByClassName('lazy-load');
-                img.onload = function() {
-                    img.style.imageRendering = 'auto';
+                let lazyImages = [].slice.call(document.querySelectorAll("img.lazy-load"));
+                let active = false;
+
+                const lazyLoad = function() {
+                    if (active === false) {
+                        active = true;
+
+                        setTimeout(function() {
+                            lazyImages.forEach(function(lazyImage) {
+                                if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+                                    lazyImage.src = lazyImage.dataset.src;
+                                    lazyImage.classList.remove("lazy-load");
+
+                                    lazyImages = lazyImages.filter(function(image) {
+                                        return image !== lazyImage;
+                                    });
+
+                                    if (lazyImages.length === 0) {
+                                        document.removeEventListener("scroll", lazyLoad);
+                                        window.removeEventListener("resize", lazyLoad);
+                                        window.removeEventListener("orientationchange", lazyLoad);
+                                    }
+                                }
+                            });
+
+                            active = false;
+                        }, 200);
+                    }
                 };
+
+                document.addEventListener("scroll", lazyLoad);
+                window.addEventListener("resize", lazyLoad);
+                window.addEventListener("orientationchange", lazyLoad);
             });
         </script>
         <?php }?>
