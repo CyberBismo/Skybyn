30d0cee73c5ab81eef79929e0b1cc9ed75d22af1
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-06-17 09:22:48 +0200

    Cleanup

--- rimeet/index.php
+++
@@ -1,8 +0,0 @@
-<?php include_once "assets/header.php";
-        if (!isset($_COOKIE['start'])) {
-            include_once "start.php";
-        }
-        include_once "dashboard.php";
-        ?>
-    </body>
-</html>
