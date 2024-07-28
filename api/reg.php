afc840d9008333d52144d0b45a20322f961f173d
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-07-28 13:44:22 +0200

    API fixed ++

--- api/reg.php
+++ api/reg.php
@@ -1,43 +1,63 @@
-<?php 
-include_once("../db_conn.php");
-$user = mysqli_real_escape_string($conn, $_POST['username']);
-$pass = mysqli_real_escape_string($conn, $_POST['password']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$error = false;
+<?php include_once("../assets/conn..php");
 
-// Check username
-if (!empty($user)) {
-    $uq = $conn->prepare("SELECT * FROM `users` WHERE `username`=?");
-    $uq->bind_param("s", $user);
-    $uq->execute();
-    $ucheck = $uq->get_result();
-    $ucount = $ucheck->num_rows;
-} else {
-    $error = true;
-}
+header('Content-Type: application/json');
 
-// Check password
-if (empty($pass)) {
-    $error = true;
-}
+if (isset($_POST['email']) && isset($_POST['reset'])) {
+    $dob = mysqli_real_escape_string($conn, $_POST['date_of_birth']);
+    $fname = mysqli_real_escape_string($conn, $_POST['first_name']);
+    $mname = mysqli_real_escape_string($conn, $_POST['middle_name']);
+    $lname = mysqli_real_escape_string($conn, $_POST['last_name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $email_c = mysqli_real_escape_string($conn, $_POST['email_c']);
+    $user = mysqli_real_escape_string($conn, $_POST['username']);
+    $pass = mysqli_real_escape_string($conn, $_POST['password']);
+    $pass_C = mysqli_real_escape_string($conn, $_POST['password_c']);
+    $refer = $_POST['refer'];
+    $ip = getIP();
+    $country = geoData("countryName");
+    $lang = geoData("countryCode");
+    $pack = $_POST['pack'];
+    $error = false;
 
-if (!$error) {
-    if ($ucount == 0) {
+    // Check username
+    if (!empty($user)) {
+        $uq = $conn->prepare("SELECT * FROM `users` WHERE `username`=?");
+        $uq->bind_param("s", $user);
+        $uq->execute();
+        $ucheck = $uq->get_result();
+        if ($ucheck->num_rows == 0) {
+            $error = false;
+        } else {
+            $error = "Username already exists.";
+        }
+    } else {
+        $error = "Username not pwovided";
+    }
+
+    // Check password
+    if (!empty($pass)) {
+        if ($pass == $pass_c) {
+            $error = false;
+        } else {
+            $error = "Paswords dont match";
+        }
+    } else {
+        $error = "Password not provided";
+    }
+
+    if ($error == false) {
         $hpw = hash("sha512", $pass);
         $salt = hash("sha512", rand());
-        $pw = hash("sha512", "$salt-$hpw");
-        $q = $conn->prepare("INSERT INTO `users` (`username`, `password`, `salt`, `email`, `verified`) VALUES (?, ?, ?, ?, '1')");
+        $pw = hash("sha512", $salt."_".$hpw);
+        $q = $conn->prepare("INSERT INTO `users` (`username`, `password`, `salt`, `email`) VALUES (?, ?, ?, ?)");
         $q->bind_param("ssss", $user, $pw, $salt, $email);
         $q->execute();
 
         $json = array("responseCode"=>"1","message"=>"Registration successful!");
         echo json_encode($json);
     } else {
-        $json = array("responseCode"=>"0","message"=>"Username already exists.");
+        $json = array("responseCode"=>"0","message"=>"$error");
         echo json_encode($json);
     }
-} else {
-    $json = array("responseCode"=>"0","message"=>"Empty details!");
-    echo json_encode($json);
 }
 ?>
--- app/index.php
+++ app/index.php
@@ -24,10 +24,23 @@
 
             .container {
                 display: flex;
-                flex-wrap: wrap;
+                justify-content: space-between;
                 gap: 10px;
+                width: 100%;
                 padding: 20px;
                 box-sizing: border-box;
+            }
+            .fullwidth {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 10px;
+                width: 100%;
+            }
+            .panel {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 10px;
+                width: auto;
             }
 
             .part {
@@ -59,136 +72,183 @@
             <h1>Skybyn - The Mobile App</h1>
         </div>
         <div class="container">
-            <div class="part">
-                <h2># Colors</h2>
-                <p>
-                    <h3>Dark Theme</h3>
-                    Main color:
+            <div class="panel">
+                <div class="part">
+                    <h2>Features</h2>
                     <ul>
-                        <li>HEX: #232325</li>
-                        <li>RGB: 35, 35, 37</li>
-                        <li><a href="https://g.co/kgs/bzuiSgQ" target="_blank">Google color picker</a></li>
-                    </ul>
-                    Other colors:
-                    <ul>
-                        <li>RGBA: 0,0,0,.# (The # may vary for each element)</li>
-                    </ul>
-                </p>
-            </div>
-            <div class="part">
-                <h2>Features</h2>
-                <ul>
-                    <li>QR (to be scanned)</li>
-                    <ul>
-                        <li>Login to website</li>
-                    </ul>
-                </ul>
-            </div>
-            <div class="part">
-                <h2>Security</h2>
-                <p>
-                    All credentials are encrypted using a one-way encryption method.<br>
-                    <br>
-                    Email are only available for you.
-                </p>
-            </div>
-            <div class="part">
-                <h2>Design</h2>
-                <p>
-                    <h3>Corners</h3>
-                    Most elements should have rounded corners. The corner radius may vary. Inner elements should have a fitted radius for it's parent.
-                    <h3>Element Backgrounds</h3>
-                    Use alpha/transparency and blur/focus difference to overlapping layers/elements. 
-                </p>
-            </div>
-            <div class="part">
-                <h2>Layout - Logged out</h2>
-                <p>
-                    <ul>
-                        <li>Loading screen (Splash screen)</li>
-                        <hr>
-                        <li>Login</li>
-                        <li>Register</li>
-                        <li>Forgot</li>
-                        <li>Reset</li>
-                    </ul>
-                </p>
-            </div>
-            <div class="part">
-                <h2>Layout - Logged in</h2>
-                <p>
-                    <ul>
-                        <li>Loading screen (Splash screen)</li>
-                        <hr>
-                        <li>Feed (main screen)</li>
-                        <li>Profile</li>
-                        <li>Friends</li>
-                        <li>Messages</li>
-                        <li>Notifications</li>
-                        <li>Group chats</li>
+                        <li>QR scanning
+                            <ul>
+                                <li>Login to website</li>
+                            </ul>
+                        </li>
+                        <li>Posting
+                            <ul>
+                                <li>Post to your public/private feed</li>
+                                <li>Post on your profile</li>
+                                <li>Post on your friends profile</li>
+                            </ul>
+                        </li>
+                        <li>Chatting
+                            <ul>
+                                <li>Chat with friends</li>
+                                <li>Create a group chat</li>
+                            </ul>
+                        </li>
                         <li>Pages</li>
-                        <li>Markets</li>
-                        <li>Games</li>
-                    </ul>
-                </p>
-            </div>
-            <div class="part">
-                <h2>API usage</h2>
-                <p>
-                    <h3>Defaults</h3>
-                    <ul>
-                        <li>URL: https://api.skybyn.com/< request >.php</li>
-                        <li>Method: POST</li>
-                        <li>Output: JSON</li>
-                    </ul>
-                    <ul>
-                        <li>Response</li>
-                        <li>
+                        <li>Notifications
                             <ul>
-                                <li>responseCode: integer (0 / 1)</li>
-                                <li>message: string (value)</li>
-                                <li>[data]: [value]</li>
+                                <li>In-app</li>
+                                <li>Device</li>
+                            </ul>
+                        </li>
+                        <li>Gaming
+                            <ul>
+                                <li>Play games</li>
+                                <li>Find people to play with</li>
+                                <li>Publish your own games</li>
+                            </ul>
+                        </li>
+                        <li>Marketing
+                            <ul>
+                                <li>Browse items</li>
+                                <li>Sell items</li>
                             </ul>
                         </li>
                     </ul>
-                </p>
+                </div>
             </div>
-            <div class="part">
-                <h2>Login API</h2>
-                <p>
-                    <h3>Request: login</h3>
-                    <ul>
-                        <li>Post variables</li>
-                        <li>
-                            <ul>
-                                <li>user = [string]</li>
-                                <li>password = [string]</li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>Success response</li>
-                        <li>
-                            <ul>
-                                <li>responseCode: 1</li>
-                                <li>message: "Welcome!"</li>
-                                <li>userID: [int]</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </p>
-            </div>
-            <div class="part">
-                <h2>Register API</h2>
-                <p>Coming soon..</p>
-            </div>
-            <div class="part">
-                <h2>Forgot API</h2>
-                <p>Coming soon..</p>
-            </div>
-            <div class="part">
-                <h2>AI</h2>
-                <p>Future feature</p>
+            <div class="fullwidth">
+                <div class="part">
+                    <h2>Security</h2>
+                    <p>
+                        <h3>General</h3>
+                        All passwords are encrypted using a one-way encryption method.<br>
+                        <br>
+                        Email are only available for you.
+                        <hr>
+                        <h3>Age</h3>
+                        All members must be at least 15 years old to have a standard account.<br>
+                        If you're under the age of 15, you must be registered by a parent, or register for account type limited.
+                        <hr>
+                        <h3>Account types</h3>
+                        <b>Standard</b> is for everyone of age 15 or older.<br>
+                        <b>Limited</b> is for everyone under the age of 15.
+                    </p>
+                </div>
+                <div class="part">
+                    <h2>Design</h2>
+                    <p>
+                        <h3>Corners</h3>
+                        Most elements should have rounded corners. The corner radius may vary. Inner elements should have a fitted radius for it's parent.
+                        <h3>Element Backgrounds</h3>
+                        Use alpha/transparency and blur/focus difference to overlapping layers/elements. 
+                    </p>
+                </div>
+                <div class="part">
+                    <h2># Colors</h2>
+                    <p>
+                        <h3>Dark Theme</h3>
+                        Main color:
+                        <ul>
+                            <li>HEX: #232325</li>
+                            <li>RGB: 35, 35, 37</li>
+                            <li><a href="https://g.co/kgs/bzuiSgQ" target="_blank">Google color picker</a></li>
+                        </ul>
+                        Other colors:
+                        <ul>
+                            <li>RGBA: 0,0,0,.# (The # may vary for each element)</li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="part">
+                    <h2>Layout - Logged out</h2>
+                    <p>
+                        <ul>
+                            <li>Loading screen (Splash screen)</li>
+                            <hr>
+                            <li>Login</li>
+                            <li>Register</li>
+                            <li>Forgot</li>
+                            <li>Reset</li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="part">
+                    <h2>Layout - Logged in</h2>
+                    <p>
+                        <ul>
+                            <li>Loading screen (Splash screen)</li>
+                            <hr>
+                            <li>Feed (main screen)</li>
+                            <li>Profile</li>
+                            <li>Friends</li>
+                            <li>Messages</li>
+                            <li>Notifications</li>
+                            <li>Group chats</li>
+                            <li>Pages</li>
+                            <li>Markets</li>
+                            <li>Games</li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="part">
+                    <h2>API usage</h2>
+                    <p>
+                        <h3>Defaults</h3>
+                        <ul>
+                            <li>URL: https://api.skybyn.com/< request >.php</li>
+                            <li>Method: POST</li>
+                            <li>Output: JSON</li>
+                        </ul>
+                        <ul>
+                            <li>Response</li>
+                            <li>
+                                <ul>
+                                    <li>responseCode: integer (0 / 1)</li>
+                                    <li>message: string (value)</li>
+                                    <li>[data]: [value]</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="part">
+                    <h2>Login API</h2>
+                    <p>
+                        <h3>Request: login</h3>
+                        <ul>
+                            <li>Post variables</li>
+                            <li>
+                                <ul>
+                                    <li>user = [string]</li>
+                                    <li>password = [string]</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>Success response</li>
+                            <li>
+                                <ul>
+                                    <li>responseCode: 1</li>
+                                    <li>message: "Welcome!"</li>
+                                    <li>userID: [int]</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="part">
+                    <h2>Register API</h2>
+                    <p>Coming soon..</p>
+                </div>
+                <div class="part">
+                    <h2>Forgot API</h2>
+                    <p>Coming soon..</p>
+                </div>
+                <div class="part">
+                    <h2>AI</h2>
+                    <p>Future feature</p>
+                </div>
             </div>
         </div>
     </body>
