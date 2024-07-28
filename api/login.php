8125c17116bc3b39dd7ccb05dfc15b55597a42bc
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-07-28 13:06:07 +0200

    API's updated

--- api/login.php
+++ api/login.php
@@ -1,32 +1,36 @@
-<?php 
-include_once("../assets/db.php");
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$pass = mysqli_real_escape_string($conn, $_POST['password']);
+<?php include_once("../assets/conn.php");
 
-$check = $conn->prepare("SELECT * FROM `users` WHERE `email`=?");
-$check->bind_param("s", $email);
-$check->execute();
-$result = $check->get_result();
-if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    $id = $row['id'];
-    $hpw = hash("sha512", $pass);
-    $salt = $row['salt'];
-    $pw = hash("sha512", $salt."_".$hpw);
-    $vcheck = $conn->prepare("SELECT * FROM `users` WHERE `id`=? AND `password`=?");
-    $vcheck->bind_param("is", $id, $pw);
-    $vcheck->execute();
-    $vresult = $vcheck->get_result();
-    
-    if ($vresult->num_rows == 1) {
-        $json = array("responseCode"=>"1","userID"=>"$id");
-        echo json_encode($json);
+header('Content-Type: application/json');
+
+if (isset($_POST['user']) && isset($_POST['password'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $check = $conn->query("SELECT * FROM `users` WHERE `email`='$user'");
+    if ($check->num_rows == 1) {
+        $row = $check->fetch_assoc();
+        $id = $row['id'];
+        $hpw = hash("sha512", $pass);
+        $salt = $row['salt'];
+        $pw = hash("sha512", $salt."_".$hpw);
+        $vcheck = $conn->prepare("SELECT * FROM `users` WHERE `id`=? AND `password`=?");
+        $vcheck->bind_param("is", $id, $pw);
+        $vcheck->execute();
+        $vresult = $vcheck->get_result();
+        
+        if ($vcheck->num_rows == 1) {
+            $json = array("responseCode"=>"1","message"=>"Welcome!","userID"=>"$id");
+            echo json_encode($json);
+        } else {
+            $json = array("responseCode"=>"0","message"=>"Wrong password");
+            echo json_encode($json);
+        }
     } else {
-        $json = array("responseCode"=>"0","message"=>"Incorrect password.");
+        $json = array("responseCode"=>"0","message"=>"Unknown email");
         echo json_encode($json);
     }
 } else {
-    $json = array("responseCode"=>"0","message"=>"Unknown email.");
+    $json = array("responseCode"=>"0","message"=>"No details provided");
     echo json_encode($json);
 }
 ?>
--- api/reset.php
+++ api/reset.php
@@ -1,11 +1,52 @@
-<?php 
-include_once('../db_conn.php');
-$reset = mysqli_real_escape_string($conn, $_POST['reset']);
+<?php include_once("../assets/conn..php");
 
-$tq = $conn->prepare("SELECT * FROM `reset_codes` WHERE `code`=?");
-$tq->bind_param("s", $reset);
-$tq->execute();
+header('Content-Type: application/json');
 
-$json = array("responseCode"=>"1","message"=>"Token stored");
-echo json_encode($json);
+if (isset($_POST['email']) && isset($_POST['reset'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $reset = mysqli_real_escape_string($conn, $_POST['reset']);
+
+    $checkCode = $conn->query("SELECT * FROM `reset_codes` WHERE `code`='$reset'");
+    if ($checkCode->num_rows == 1) {
+        $checkUser = $conn->query("SELECT * FROM `users` WHERE `email`='$email'");
+        if ($checkUser->num_rows == 1) {
+            $resetData = $checkCode->fetch_assoc();
+            $userid = $userData['userid'];
+            $userData = $checkUser->fetch_assoc();
+            $uid = $userData['id'];
+
+            if ($userid == $uid) {
+                $json = array(
+                    "responseCode"=>"1",
+                    "message"=>"Reset code verified"
+                );
+                echo json_encode($json);
+            } else {
+                $json = array(
+                    "responseCode"=>"0",
+                    "message"=>"Incorrect code"
+                );
+                echo json_encode($json);
+            }
+        } else {
+            $json = array(
+                "responseCode"=>"0",
+                "message"=>"Unknown email"
+            );
+            echo json_encode($json);
+        }
+    } else {
+        $json = array(
+            "responseCode"=>"0",
+            "message"=>"Invalid code"
+        );
+        echo json_encode($json);
+    }
+} else {
+    $json = array(
+        "responseCode"=>"0",
+        "message"=>"Required information not provided"
+    );
+    echo json_encode($json);
+}
 ?>
