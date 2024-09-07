4c9e0b6deadc4c53dff88e6687a380d713f9c621
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-09-07 22:26:56 +0200

    Update ccLog.php

--- assets/ccLog.php
+++ assets/ccLog.php
@@ -1,17 +1,20 @@
 <?php if (isset($_GET['x'])) {?>
 <script>
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'cleanClientLog.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response.message);
-            setTimeout(() => {
-                readClientInfoLog();
-            }, 10000);
-        }
-    };
-    xhr.send();
+    function readClientInfoLog() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'cleanClientLog.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                console.log(response.message);
+                setTimeout(() => {
+                    readClientInfoLog();
+                }, 10000);
+            }
+        };
+        xhr.send();
+    }
+    readClientInfoLog();
 </script>
 <?php }?>
