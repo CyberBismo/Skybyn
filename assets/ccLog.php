fcf254543b87c439394fe75f27f9a64c91905a5f
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-09-07 22:38:42 +0200

    Update ccLog.php

--- assets/ccLog.php
+++ assets/ccLog.php
@@ -1,5 +1,12 @@
 <?php if (isset($_GET['x'])) {?>
 <script>
+    function clock() {
+        const time = new Date().toLocaleTimeString();
+        document.getElementById('time').innerHTML = `<p>${time}</p>`;
+        setTimeout(() => {
+            clock();
+        }, 1000);
+    }
     function readClientInfoLog() {
         var xhr = new XMLHttpRequest();
         xhr.open('POST', 'cleanClientLog.php', true);
@@ -17,7 +24,9 @@
         };
         xhr.send();
     }
+    clock();
     readClientInfoLog();
 </script>
+<div id="time"></div>
 <div id="status"></div>
 <?php }?>
