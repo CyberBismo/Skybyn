b813b882d1b9f455e19db83c1333fbfad88a6ad5
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-09-07 21:04:37 +0200

    Changes

--- assets/ccLog.php
+++ assets/ccLog.php
@@ -1,22 +1,17 @@
 <?php if (isset($_GET['x'])) {?>
 <script>
-    function cleanClientLog() {
-        const password = prompt('Enter password to clean client log:');
-        if (password == null) {
-            return;
-        }
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'cleanClientLog.php', true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'cleanClientLog.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response.message);
             setTimeout(() => {
                 readClientInfoLog();
             }, 10000);
-            }
-        };
-        xhr.send();
-    }
-    cleanClientLog();
+        }
+    };
+    xhr.send();
 </script>
 <?php }?>
--- assets/cleanClientLog.php
+++ assets/cleanClientLog.php
@@ -8,18 +8,33 @@ $clients = json_decode($fileContent, true);
 $currentTimestamp = time();
 $fiveMinutesAgo = $currentTimestamp - (5 * 60);
 
+$cleaned = false; // Variable to track if any entries were cleaned
+
 foreach ($clients as &$client) {
     for ($i = 0; $i < count($client); $i++) {
         $clientTimestamp = strtotime($client[$i]['time']);
-        if ($clientTimestamp <= $fiveMinutesAgo) {
-            $client[$i]['time'] = $client[$i]['time'];
-        } else {
+        if ($clientTimestamp >= $fiveMinutesAgo) {
             unset($client[$i]);
+            $cleaned = true; // Set cleaned to true if any entry is removed
         }
     }
-    break;
 }
 
 $fileContent = json_encode($clients, JSON_PRETTY_PRINT);
 file_put_contents($path.$file, $fileContent);
+
+if ($cleaned) {
+    $data = [
+        'status' => 'success',
+        'message' => 'Client log cleaned successfully'
+    ];
+} else {
+    $data = [
+        'status' => 'no_changes',
+        'message' => 'No entries were cleaned'
+    ];
+}
+
+header('Content-Type: application/json');
+echo json_encode($data);
 ?>
