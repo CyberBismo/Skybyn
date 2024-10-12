637dcdb6c889e25b59091d8da345742c890c191b
Lars Einar Bismo <84242553+CyberBismo@users.noreply.github.com>
2024-10-13 00:34:41 +0200

    Update login.php

--- assets/forms/login.php
+++ assets/forms/login.php
@@ -48,8 +48,7 @@
                     <h2 id="normal_login_header">Sign in</h2>
 
                     <i class="fa-solid fa-at"></i>
-                    <input type="email" id="login-email" onkeydown="hitEnterLogin(this)" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required placeholder="E-mail address" title="example@example.com" oninput="setCustomValidity('')" oninvalid="setCustomValidity('Please enter a valid email address')" autofocus>
-
+                    <input type="email" id="login-email" onkeydown="hitEnterLogin(this)" pattern="[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$" required placeholder="E-mail address" title="example@example.com" oninput="setCustomValidity('')" oninvalid="setCustomValidity('Please enter a valid email address')" autofocus>
                     <i class="fa-solid fa-key"></i>
                     <input type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,}$" id="login-password" onkeydown="hitEnterLogin(this)" placeholder="Password" oninvalid="setCustomValidity('Password must be at least 8 characters long, with at least one lowercase letter, one uppercase letter, and one digit.')" required>
                     <i class="fa-regular fa-eye" onclick="showPassword('login-password')"></i>
