18a5e749f5dc41221f46b86060f5ab67cfd64b30
CyberByte <84242553+CyberBismo@users.noreply.github.com>
2025-06-01 23:45:53 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,25 @@
 <?php
+$to = isset($_POST['to_email']) ? $_POST['to_email'] : '';
+$subject = 'Test Email';
+$message = 'This is a test email sent to ' . htmlspecialchars($to) . '.';
+$headers = 'From: noreply@skybyn.no' . "\r\n" .
+           'Reply-To: noreply@skybyn.no' . "\r\n" .
+           'X-Mailer: PHP/' . phpversion();
+?>
 
-phpinfo();
+<form method="post">
+    <label>Emails will be sent from <b>noreply@skybyn.no</b></label><br><br>
+    <label for="to_email">To Email:</label><br>
+    <input type="email" id="to_email" name="to_email" required><br>
+    <button type="submit">Send Email</button>
+</form>
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'Email sent successfully to ' . htmlspecialchars($to);
+    } else {
+        echo 'Email sending to ' . htmlspecialchars($to) . ' failed.';
+    }
+}
 ?>
